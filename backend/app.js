c3e7dade2b47b553daa0fbbe477d1f6ace396ef9
mansi joshi <mansi.joshi@yh.nackademin.se>
2025-11-02 10:30:20 +0100

    added port 3306

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,16 @@ app.use(express.json());
 app.get("/", async (req, res) => {
     try {
         const [rows] = await pool.query("SELECT 1 AS test");
-        res.send("My sql coonected successfully");
+        res.send("<h2> Hotel Booking Backend is Live — Database Connected Successfully!</h2>");
     } catch (err){
-        res.status(500).send("database connection failed");
+        res.status(500).send("<h2>Database connection failed</h2>");
         console.error(err);
     }
 });
 
 app.use("/api", bookingRoutes);
 
-app.listen(3000, () => console.log("server running on http://localhost:3000"));
+//app.listen(3000, () => console.log("server running on http://localhost:3000"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
