d3ebdc31658045bc4596bafefe836cf53f7a4c86
mansi joshi <mansi.joshi@yh.nackademin.se>
2025-11-02 09:36:53 +0100

    Added .gitignore to ignore env and node_modules, also changed database details for free database to show the live demo through Render

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise" ;
+import dotenv from "dotenv";
+dotenv.config();
 
 // 1: to connect to mysql server
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Anujoshi@1",
-    database: "hotel_database",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 });
 
 export default pool;
