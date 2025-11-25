ca59c033f92659e3e305cb32a805ec856ce4b94e
Mansi Joshi <mansijoshiin80@gmail.com>
2025-11-25 14:39:23 +0100

    changes done before deployment

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 // 1: to connect to mysql server
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "Anujoshi@1",
-  database: process.env.DB_NAME || "hotel_database",
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
